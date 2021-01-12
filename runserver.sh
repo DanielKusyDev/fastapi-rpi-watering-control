909ddfc0ddb4f40a02eecd97d729ce1414570b11
Daniel Kusy <daniel.kusy@embiq.com>
2021-01-12 23:12:07 +0100

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-baseDir="/home/pi/Projects/fastapi-rpi-watering-control"
-source ~/.virtualenvs/plants/bin/activate
+baseDir="~/Projects/fastapi-rpi-watering-control/app"
+source "~/.virtualenvs/plants/bin/activate"
 
 export PYTHONPATH="$baseDir"
-cd "$baseDir/app"
+cd "$baseDir"
 
 alembic upgrade head
-python "$baseDir/app/main.py"
+python "$baseDir/main.py"
