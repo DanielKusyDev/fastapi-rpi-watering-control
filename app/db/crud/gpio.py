1a9898e7c79c7a8801c590a12136b548658a8a14
Daniel Kusy <daniel.kusy@embiq.com>
2021-01-31 20:13:52 +0100

    Fix gpio channel name.

--- app/db/crud/gpio.py
+++ app/db/crud/gpio.py
@@ -15,7 +15,7 @@ def get_gpio_pins() -> List[models.Gpio]:
 def set_gpio_pin_state(channel: int, state: bool):
     with Connection.session_scope() as db:
         try:
-            gpio_input: models.Gpio = db.query(models.Gpio).filter_by(pin=channel).one()
+            gpio_input: models.Gpio = db.query(models.Gpio).filter_by(channel=channel).one()
         except BaseException as e:
             logger.error(str(e))
             raise e
