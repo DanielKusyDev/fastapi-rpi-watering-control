f3251893bdc7277f3d9b421472ac345536a91cd9
Daniel Kusy <daniel.kusy@embiq.com>
2021-01-31 20:04:14 +0100

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-baseDir="./app"
 source "$VENV_PATH"
-
-export PYTHONPATH="$baseDir"
-cd "$baseDir"
-
-alembic upgrade head
-python "$baseDir/main.py"
+cd app
+export PYTHONPATH="$PWD"
+alembic upgrade head && python main.py
